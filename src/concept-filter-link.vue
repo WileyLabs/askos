eb591ce9995bf9987d1a7b8c7d13d0bd50e9327e
Benjamin Young <byoung2@wiley.com>
2017-08-07 10:55:34 -0400

    Add missing Concept filter link file...

---
+++ src/concept-filter-link.vue
@@ -0,0 +1,19 @@
+<template>
+  <a :href="resource" @click.prevent="selected()">
+    {{resource | curie}}
+    <i class="filter icon"></i>
+  </a>
+</template>
+
+<script>
+export default {
+  props: ['resource'],
+  methods: {
+    selected() {
+      this.$store.commit('setActiveConcept', {
+        concept: this.resource
+      });
+    }
+  }
+}
+</script>

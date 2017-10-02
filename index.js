9fb3b0b7436fe4720504feb976f15e3fce81c279
Benjamin Young <byoung2@wiley.com>
2017-10-02 10:23:36 -0400

    Use dcterms to match RDFa Initial Context
    https://www.w3.org/2013/json-ld-context/rdfa11
    
    Also alphabetize

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ require('isomorphic-fetch');
 
 let context = {
   "@context": {
-    "dct": "http://purl.org/dc/terms/",
-    "owl": "http://www.w3.org/2002/07/owl#",
-    "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-    "xml": "http://www.w3.org/XML/1998/namespace",
-    "xsd": "http://www.w3.org/2001/XMLSchema#",
+    "dcterms": "http://purl.org/dc/terms/",
     "foaf": "http://xmlns.com/foaf/0.1/",
+    "owl": "http://www.w3.org/2002/07/owl#",
     "prov": "http://www.w3.org/ns/prov#",
+    "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
     "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
     "skos": "http://www.w3.org/2004/02/skos/core#",
+    "xml": "http://www.w3.org/XML/1998/namespace",
+    "xsd": "http://www.w3.org/2001/XMLSchema#",
     "@base": "https://vocabulary.wiley.com/PublicationType/",
     "@language": "en"
   }
